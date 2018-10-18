4a8d8765986adcde38d453ebb2e8c2f14e6c2733
Dennis Ruhe <dennis@amazingsystems.nl>
2018-10-18 14:32:23 +0200

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-require('lib.finder')
 require('room');
 require('structure.spawn')
 
--- room.js
+++ room.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Finder = require('lib.finder')
+
 Room.prototype.tick = function () {
     Finder.findIdleSpawner(this).spawnMiner()
 }
