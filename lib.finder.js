c089cf13aa33f3ba3518b0becafa909158f27cd1
Dennis Ruhe <dennis@amazingsystems.nl>
2018-10-18 15:48:34 +0200

    Energies

--- lib.finder.js
+++ lib.finder.js
@@ -1,6 +1,18 @@
 'use strict';
 
 class Finder {
+    static energies(room) {
+        let result = _.filter(room.find(FIND_DROPPED_RESOURCES), function(r) {
+            return r.amount >= Config.minEnergy[room.level()] && r.resourceType === RESOURCE_ENERGY
+        })
+        if (result.length > 0) return result
+        result = _.filter(room.find(FIND_STRUCTURES), e => {return e.structureType == STRUCTURE_CONTAINER && e.store[RESOURCE_ENERGY] > Config.minEnergy[room.level()]})
+        if (result.length > 0) return result
+        result = _.filter(room.find(FIND_STRUCTURES), e => {return e.structureType == STRUCTURE_STORAGE && e.store[RESOURCE_ENERGY] > 0})
+        if (result.length > 0) return result
+        return null
+    }
+
     static findCreepsWithTarget(key, e) {
         return _.filter(Game.creeps, function (c) {
             return c.my && c.memory.task !== 'restore' && c.targetIs(key, target)
