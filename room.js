0d563ca098aa0161dbe0a7da0e8e3bbe1b4c08f2
Dennis Ruhe <dennis@amazingsystems.nl>
2018-10-18 15:11:54 +0200

    Updates

--- room.js
+++ room.js
@@ -3,15 +3,15 @@
 const Finder = require('lib.finder')
 
 Room.prototype.tick = function () {
-    if (this.needMiners()) {
-        Finder.findIdleSpawner(this).spawnMiner()
-    } else if (this.needHaulers()) {
+    if (this.needHaulers()) {
         Finder.findIdleSpawner(this).spawnHauler()
+    } else if (this.needMiners()) {
+        Finder.findIdleSpawner(this).spawnMiner()
     }
 }
 Room.prototype.needMiners = function () {
     return Finder.findMyCreepsByTask(this, 'miner').length < this.find(FIND_SOURCES).length
 }
 Room.prototype.needHaulers = function () {
-    return Finder.findMyCreepsByTask(this, 'hauler').length < 3
+    return Finder.findMyCreepsByTask(this, 'hauler').length < Finder.findMyCreepsByTaks(this, 'miner')
 }
